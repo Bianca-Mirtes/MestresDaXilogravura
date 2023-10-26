68d8d0226165ab0206d258da82e4e9c34e8756d7
Bianca Miranda <miranda.bianca41@gmail.com>
2023-10-26 08:06:46 -0300

    Ambientação e Teletransporte

---
+++ Assets/collisionController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionController : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    /*void FixedUpdate()
+    {
+        Vector3 newPosition = cam.position;
+        transform.position = newPosition + new Vector3(0f, 0f, 0f);
+    }*/
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("parede"))
+        {
+            rb.AddForce(Vector3.back*10000);
+        }
+    }
+}
