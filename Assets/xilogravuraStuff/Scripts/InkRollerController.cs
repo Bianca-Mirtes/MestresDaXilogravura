981cf73c7bfecbc7d64b374d7529b69d00c66a1c
Samuel981 <ssoares981@gmail.com>
2023-11-14 16:47:38 -0300

    adiciona mudança de cor no rolinho ao pegar tinta

--- Assets/xilogravuraStuff/Scripts/InkRollerController.cs
+++ Assets/xilogravuraStuff/Scripts/InkRollerController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class InkRollerController : MonoBehaviour
 {
     private bool isInk = false;
+    public Material borracha;
     // Start is called before the first frame update
     void Start()
     {
-        
+        borracha.SetFloat("isTinta", 0);
     }
 
     // Update is called once per frame
@@ -20,6 +21,7 @@ public class InkRollerController : MonoBehaviour
     public void enableInk()
     {
         isInk = true;
+        borracha.SetFloat("isTinta", 1);
     }
 
     public bool isInkEnable()
